4d2bc119c536a21612c1cf7c2af520edd2d831e8
monzter50 <josemonzter50@gmail.com>
2019-02-20 17:51:48 -0500

    tercera tarea del curso

---
+++ mito.js
@@ -0,0 +1,15 @@
+
+let subcriptores = 22000;
+
+// module.exports.saludar = function saludar(){
+// 	console.log('subcribete al canal');
+
+// };
+// module.exports.subcriptores = 22000;
+
+module.exports = {
+	subs:subcriptores,
+	saludar:function(){
+		console.log('subcribete al canal');
+	}
+};
